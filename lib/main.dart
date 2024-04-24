135ce13261f84f721f99cab274ceb72da3fb274a
nchola <108863217+nchola@users.noreply.github.com>
2024-04-24 15:28:12 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:muhammadnanda_uts/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
